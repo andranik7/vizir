f4d11fd5d08e90ad6fcbe5d8f9314efa0f78e9bb
Andranik <andranikhayra@gmail.com>
2019-05-09 23:56:33 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+let key = "x9F6yyXvxdSqqj0rN0L0kFDVRBWimmAe" // clé api Giphy
+// Require with custom API key
+let giphy = require('giphy-api')(key);
+
+
+
+
+
+// router pour récupérer la requête post depuis Slack
+router.post('/randomGif', function(req, res, next) {
+  let command = req.body.command;
+  let text = req.body.text;
+
+  // faire une recherche giphy avec l'api
+  giphy.search(text, function (err, data) {
+    // si tout va bien
+    if(!err){
+      let gifs = data.data
+      res.writeHead(200, {'Content-Type': 'image/gif' });
+      console.log(gifs[0].images.fixed_height.url)
+      res.end(gifs[0].images.fixed_height.url);
+    }else{
+      res.send("Pas de gif trouvé :(")
+    }
+  });
+
+});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.post('/randomGif', function(req, res, next) {
-  console.log(req.body.command)
-  console.log(req.body.text)
-  res.send("hello")
 });
 
 
